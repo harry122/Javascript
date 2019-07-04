af34d86704d83afa22aa953ed26fa7dcd5ba3c50
harry122 <mehra.harish7@gmail.com>
2019-07-04 12:49:58 +0530

    files added

---
+++ operators.js
@@ -0,0 +1,19 @@
+// Assignment Operator
+// Assignment =
+// Plus Assignment
+// subtract aasignment
+// modulus 
+
+var a = 10;
+console.log(a);
+
+// Plus Assignment Operator
+
+var foo = 10;
+foo += 10;
+console.log(foo);
+
+// Subtract assignment operator
+var foo1 = 20;
+foo1-= 10;
+console.log(foo1);
---
+++ variables.js
@@ -0,0 +1,28 @@
+
+// Declaration
+
+var totalPrice;  // declaration statememt
+totalPrice = 1000; // assignment
+
+console.log(totalPrice);
+
+var discontPrice = 750;   
+
+console.log("Discount Price :", discontPrice);
+
+var savingData;
+
+console.log(savingData); // undefined
+
+//     console.log(myValue); // not define
+
+var _ = 15;
+console.log(_);
+
+// Camel casing
+
+var totalPriceOfProduct = 40;
+
+// Underscore
+
+var total_price_of_product;
