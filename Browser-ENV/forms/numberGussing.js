e20e7cd28cc8ee58452f5fefc4e6787f63da68cb
harry122 <mehra.harish7@gmail.com>
2019-08-10 16:39:40 +0530

    some prgrams

--- Browser-ENV/forms/numberGussing.js
+++ Browser-ENV/forms/numberGussing.js
@@ -1,11 +1,6 @@
 let frm = document.querySelector(".form");
 
-function randomN(max,min){
 
-    console.log(Math.floor(Math.random * min);
-
-}
- randomN(1,99);
 
 frm.addEventListener('click',function(e){
 
